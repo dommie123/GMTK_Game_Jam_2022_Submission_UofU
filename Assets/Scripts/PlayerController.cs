3aab336c5b4f2360a5173185f40cf88da4e5deab
Flykes <u1394007@utah.edu>
2022-07-16 02:20:54 -0600

    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float gravity;
     [SerializeField] private float mouseSensitivity;
-    private Vector3 gravityVector;
-    public float gravity;
 
     private Transform playerBody;
-    private Vector3 stickToGround;
     private CharacterController controller;
     private float xRotation;
-    private Vector3 playerRotation;
+
     public Transform playerCamera;
+
     public Transform groundCheck;
     public float groundDistance;
     public LayerMask groundMask;
@@ -26,7 +25,6 @@ public class PlayerController : MonoBehaviour
     void Awake() 
     {
         playerBody = this.transform;
-        playerRotation = new Vector3(0, 0, 0);
         controller = GetComponent<CharacterController>();
         xRotation = 0f;
         Cursor.lockState = CursorLockMode.Locked;
@@ -38,9 +36,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        stickToGround = GetGroundVelocity();
-        gravityVector = GetGravityVector();
-        //Debug.Log(gravityVector);
 
         UpdateMovement();
         UpdateRotation();
@@ -56,6 +51,7 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Jump");
             Jump();
         }
+        
     }
 
     private void UpdateMovement()
@@ -69,14 +65,6 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateRotation()
     {
-        // Set the player rotation once with each change of gravity.
-        if (GetPlayerRotation() != playerRotation)
-        {
-            playerRotation = GetPlayerRotation();
-            transform.localRotation = Quaternion.Euler(playerRotation.x, playerRotation.y, playerRotation.z);
-        }
-
-        // FPS Camera stuff
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -89,14 +77,13 @@ public class PlayerController : MonoBehaviour
 
     private void UpdatePhysics()
     {
-        // Check to stick player to ground if they are grounded, unless they decide to jump. Otherwise, laws of physics apply.
-        if (isGrounded && PlayerVelocityIsIncreasing())
+        if (isGrounded && velocity.y < 0)
         {
-            velocity = stickToGround;
+            velocity.y = -2f;
         }
         else
         {
-            velocity += gravityVector * Time.deltaTime;
+            velocity.y += gravity * Time.deltaTime;;
         }
 
         controller.Move(velocity * Time.deltaTime);
@@ -110,114 +97,7 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        switch (GravitySwitcher.instance.direction)
-        {
-            case GravitySwitcher.GravityDirection.UP:
-                velocity.y = -Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            case GravitySwitcher.GravityDirection.DOWN:
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            case GravitySwitcher.GravityDirection.LEFT:
-                velocity.x = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            case GravitySwitcher.GravityDirection.RIGHT:
-                velocity.x = -Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            case GravitySwitcher.GravityDirection.FORWARD:
-                velocity.z = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            case GravitySwitcher.GravityDirection.BACKWARD:
-                velocity.z = -Mathf.Sqrt(jumpHeight * -2f * gravity);
-                break;
-            default:
-                Debug.LogError($"{GravitySwitcher.instance.direction} is not a valid direction!");
-                break;
-        }
-        Debug.Log(velocity);
-    }
-
-    private Vector3 GetGroundVelocity()
-    {
-        switch (GravitySwitcher.instance.direction)
-        {
-            case GravitySwitcher.GravityDirection.UP:
-                return new Vector3(0, 2f, 0);
-            case GravitySwitcher.GravityDirection.DOWN:
-                return new Vector3(0, -2f, 0);
-            case GravitySwitcher.GravityDirection.LEFT:
-                return new Vector3(-2f, 0, 0);
-            case GravitySwitcher.GravityDirection.RIGHT:
-                return new Vector3(2f, 0, 0);
-            case GravitySwitcher.GravityDirection.FORWARD:
-                return new Vector3(0, 0, -2f);
-            case GravitySwitcher.GravityDirection.BACKWARD:
-                return new Vector3(0, 0, 2f);
-            default:
-                return new Vector3(0, 0, 0);   
-        }
-    }
-
-    private Vector3 GetGravityVector()
-    {
-        switch (GravitySwitcher.instance.direction)
-        {
-            case GravitySwitcher.GravityDirection.UP:
-                return new Vector3(0, -gravity, 0);
-            case GravitySwitcher.GravityDirection.DOWN:
-                return new Vector3(0, gravity, 0);
-            case GravitySwitcher.GravityDirection.LEFT:
-                return new Vector3(gravity, 0, 0);
-            case GravitySwitcher.GravityDirection.RIGHT:
-                return new Vector3(-gravity, 0, 0);
-            case GravitySwitcher.GravityDirection.FORWARD:
-                return new Vector3(0, 0, gravity);
-            case GravitySwitcher.GravityDirection.BACKWARD:
-                return new Vector3(0, 0, -gravity);
-            default:
-                return new Vector3(0, 0, 0);   
-        }
-    }
-
-    private Vector3 GetPlayerRotation()
-    {
-        switch (GravitySwitcher.instance.direction)
-        {
-            case GravitySwitcher.GravityDirection.UP:
-                return new Vector3(180, 0, 0);
-            case GravitySwitcher.GravityDirection.DOWN:
-                return Vector3.zero;
-            case GravitySwitcher.GravityDirection.LEFT:
-                return new Vector3(0, 0, -90);
-            case GravitySwitcher.GravityDirection.RIGHT:
-                return new Vector3(0, 0, 90);
-            case GravitySwitcher.GravityDirection.FORWARD:
-                return new Vector3(90, 0, 0);
-            case GravitySwitcher.GravityDirection.BACKWARD:
-                return new Vector3(-90, 0, 0);
-            default:
-                return new Vector3(0, 0, 0);   
-        }
-    }
-
-    private bool PlayerVelocityIsIncreasing()
-    {
-        switch (GravitySwitcher.instance.direction)
-        {
-            case GravitySwitcher.GravityDirection.UP:
-                return velocity.y > 0;
-            case GravitySwitcher.GravityDirection.DOWN:
-                return velocity.y < 0;
-            case GravitySwitcher.GravityDirection.LEFT:
-                return velocity.x < 0;
-            case GravitySwitcher.GravityDirection.RIGHT:
-                return velocity.x > 0;
-            case GravitySwitcher.GravityDirection.FORWARD:
-                return velocity.z < 0;
-            case GravitySwitcher.GravityDirection.BACKWARD:
-                return velocity.z > 0;
-            default:
-                return false;   
-        }
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        Debug.Log(velocity.y);
     }
 }
